2968776ad612764b9e7b618cb93075001f0c6dc9
Kaike Renan de Almeida <kaikerenan11@gmail.com>
2026-04-18 09:06:21 -0300

    feat: implemented initial project's structure
    project following the DDD architecture

---
+++ ProjetoP2/Domain/Entities/Clinics.cs
@@ -0,0 +1,6 @@
+﻿namespace ProjetoP2.Domain.Entities
+{
+    public class Clinics
+    {
+    }
+}
---
+++ ProjetoP2/Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjetoP2.Domain.Entities
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }   
+        public string PhoneNumber { get; set; }
+
+    }
+}
